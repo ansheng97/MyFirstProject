611dc293128b788eb77a0cd2466fe21c53920364
anshe <ansheng97@163.com>
2026-06-16 16:32:31 +0800

    第二次提交

--- src/com/demo/Main.java
+++ src/com/demo/Main.java
@@ -1,10 +1,20 @@
 package com.demo;
 
+
+import java.util.ArrayList;
+
 public class Main {
     public  static void main(String[] args){
         int a =10;
         int b = 20;
         System.out.println(a+b);
-
+        System.out.println("Hello World");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("次数："+i);
+        }
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Java");
+        list.add("IDEA");
+        System.out.print(list);
     }
 }
